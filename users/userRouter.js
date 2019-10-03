1aa317bb00598491389a348602f31bff0cabbb1a
Anthony <anthony.kizer88@gmail.com>
2019-10-02 23:50:43 -0700

    finished stretch

--- users/userRouter.js
+++ users/userRouter.js
@@ -8,7 +8,19 @@ router.post("/", validateUser, (req, res) => {
   res.status(201).json(req.body);
 });
 
-router.post("/:id/posts", validateUserId, validatePost, (req, res) => {});
+router.post("/:id/posts", validatePost, validateUserId, async (req, res) => {
+  try {
+    const id = req.params.id;
+    const newPost = { ...req.body, user_id: id };
+    await Post.insert(newPost);
+    res.status(201).json(newPost);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Error processing request"
+    });
+  }
+});
 
 router.get("/", (req, res) => {
   User.get(req.query)
@@ -30,6 +42,7 @@ router.get("/:id", validateUserId, (req, res) => {
 router.get("/:id/posts", validateUserId, async (req, res) => {
   try {
     const userPosts = await User.getUserPosts(req.params.id);
+    console.log(userPosts);
     res.status(200).json(userPosts);
   } catch (error) {
     console.log(error);
@@ -47,7 +60,22 @@ router.delete("/:id", validateUserId, async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {});
+router.put("/:id", validateUserId, async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const changes = req.body;
+
+    if (changes) {
+      await User.update(id, changes);
+      res.status(200).json({ message: "username updated" });
+    } else {
+      res.status(400).json({ message: "please enter text into field" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "could not process request" });
+  }
+});
 
 //custom middleware
 
@@ -71,6 +99,8 @@ async function validateUser(req, res, next) {
     if (req.body.name.length > 0) {
       const newUser = await User.insert(req.body);
       res.status(201).json(newUser);
+      console.log("validated");
+      next();
     } else {
       res.status(400).json({
         errorMessage: "Please provide a username"
@@ -84,6 +114,18 @@ async function validateUser(req, res, next) {
   }
 }
 
-function validatePost(req, res, next) {}
+async function validatePost(req, res, next) {
+  try {
+    if (req.body.text.length > 0) {
+      console.log("validatedpost");
+      next();
+    } else {
+      res.status(400).json({ errorMessage: "text field was blank" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "error processing request" });
+  }
+}
 
 module.exports = router;
